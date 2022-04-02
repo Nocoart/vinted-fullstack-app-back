95d145527767dacfb74c2c83805812f0a4d56782
Nicolas Guerineau <nicolasguerineau@MacBook-Air.com>
2022-04-02 18:14:32 +0200

    fix new user route, clean-up

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,131 +14,122 @@ const isAuthenticated = require("../utils/middleWares");
 // SIGNUP
 
 router.post("/user/signup", async (req, res) => {
-	try {
-		if (await User.findOne({ email: req.fields.email.toLocaleLowerCase() })) {
-			res.status(400).json({ error: `This email already exists` });
-		} else if (!req.fields.username || !req.fields.email) {
-			res.status(400).json({ error: `email or username missing` });
-		} else {
-			if (!verifyEmail(req.fields.email)) {
-				res.status(400).json({ error: `Invalid email input` });
-			} else {
-				//Account setup
+  try {
+    if (await User.findOne({ email: req.fields.email.toLocaleLowerCase() })) {
+      res.status(400).json({ error: `This email already exists` });
+    } else if (!req.fields.username || !req.fields.email) {
+      res.status(400).json({ error: `email or username missing` });
+    } else {
+      if (!verifyEmail(req.fields.email)) {
+        res.status(400).json({ error: `Invalid email input` });
+      } else {
+        //Account setup
 
-				const salt = uid2(16);
-				const token = uid2(16);
-				const hash = SHA256(req.fields.password + salt).toString(encBase64);
+        const salt = uid2(16);
+        const token = uid2(16);
+        const hash = SHA256(req.fields.password + salt).toString(encBase64);
 
-				const newUser = new User({
-					email: req.fields.email.toLocaleLowerCase(),
-					account: {
-						username: req.fields.username,
-						phone: req.fields.phone,
-					},
-					token: token,
-					hash: hash,
-					salt: salt,
-				});
+        const newUser = new User({
+          email: req.fields.email.toLocaleLowerCase(),
+          account: {
+            username: req.fields.username,
+            phone: req.fields.phone,
+          },
+          token: token,
+          hash: hash,
+          salt: salt,
+        });
+        console.log("rigth place");
+        if (req.files.picture) {
+          console.log(req.files.picture);
+          const pictureToUpload = req.files.picture.path;
+          const savedPicture = await cloudinary.uploader.upload(pictureToUpload, {
+            folder: `/vinted/users/${newUser._id}`,
+          });
+          console.log(savedPicture);
+          newUser.account.avatar = savedPicture;
+          console.log(newUser);
+        }
 
-				if (req.files) {
-					console.log(req.files.picture);
-					const pictureToUpload = req.files.picture.path;
-					const savedPicture = await cloudinary.uploader.upload(
-						pictureToUpload,
-						{
-							folder: `/vinted/users/${newUser._id}`,
-						}
-					);
-					console.log(savedPicture);
-					newUser.account.avatar = savedPicture;
-					console.log(newUser);
-				}
+        await newUser.save();
 
-				await newUser.save();
+        // Response
 
-				// Response
-
-				res.status(200).json({
-					id: newUser._id,
-					email: newUser.email,
-					username: newUser.account.username,
-					phone: newUser.account.phone,
-					token: newUser.token,
-				});
-			}
-		}
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
+        res.status(200).json({
+          id: newUser._id,
+          email: newUser.email,
+          username: newUser.account.username,
+          phone: newUser.account.phone,
+          token: newUser.token,
+        });
+      }
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 //LOGIN
 
 router.post("/user/login", async (req, res) => {
-	const email = req.fields.email;
-	const foundUser = await User.findOne({ email: email.toLocaleLowerCase() });
-	if (!foundUser) {
-		res.status(400).json({ message: `Email and password do not match` });
-	} else {
-		const newHash = SHA256(req.fields.password + foundUser.salt).toString(
-			encBase64
-		);
-		if (newHash === foundUser.hash) {
-			res.status(200).json({
-				_id: foundUser._id,
-				token: foundUser.token,
-				account: foundUser.account,
-			});
-		} else {
-			res.status(400).json({ message: `Email and password do not match` });
-		}
-	}
+  const email = req.fields.email;
+  const foundUser = await User.findOne({ email: email.toLocaleLowerCase() });
+  if (!foundUser) {
+    res.status(400).json({ message: `Email and password do not match` });
+  } else {
+    const newHash = SHA256(req.fields.password + foundUser.salt).toString(encBase64);
+    if (newHash === foundUser.hash) {
+      res.status(200).json({
+        _id: foundUser._id,
+        token: foundUser.token,
+        account: foundUser.account,
+      });
+    } else {
+      res.status(400).json({ message: `Email and password do not match` });
+    }
+  }
 });
 
 //UPDATE
 
 router.post("/user/update", isAuthenticated, async (req, res) => {
-	try {
-		const foundUser = await User.findById(req.fields.id);
-		if (!foundUser) {
-			res.status(400).json({ error: `Id missing or incorect` });
-		} else {
-			// Update image
-			if (req.files.picture) {
-				await cloudinary.api.delete_resources_by_prefix(
-					`${foundUser.account.avatar.public_id}`
-				);
+  try {
+    const foundUser = await User.findById(req.fields.id);
+    if (!foundUser) {
+      res.status(400).json({ error: `Id missing or incorect` });
+    } else {
+      // Update image
+      if (req.files.picture) {
+        await cloudinary.api.delete_resources_by_prefix(`${foundUser.account.avatar.public_id}`);
 
-				const pictureToUpload = req.files.picture.path;
-				const savedPicture = await cloudinary.uploader.upload(pictureToUpload, {
-					folder: `/vinted/users/${foundUser._id}`,
-				});
+        const pictureToUpload = req.files.picture.path;
+        const savedPicture = await cloudinary.uploader.upload(pictureToUpload, {
+          folder: `/vinted/users/${foundUser._id}`,
+        });
 
-				foundUser.account.avatar = savedPicture;
-			}
-			// Update fields infos if present
-			if (req.fields.email) {
-				foundUser.email = req.fields.email;
-			}
-			if (req.fields.username) {
-				foundUser.account.username = req.fields.username;
-			}
-			if (req.fields.phone) {
-				foundUser.account.phone = req.fields.phone;
-			}
-			if (req.fields.password) {
-				foundUser.hash = SHA256(req.fields.password + foundUser.salt).toString(
-					encBase64
-				);
-			}
+        foundUser.account.avatar = savedPicture;
+      }
+      // Update fields infos if present
+      if (req.fields.email) {
+        foundUser.email = req.fields.email;
+      }
+      if (req.fields.username) {
+        foundUser.account.username = req.fields.username;
+      }
+      if (req.fields.phone) {
+        foundUser.account.phone = req.fields.phone;
+      }
+      if (req.fields.password) {
+        foundUser.hash = SHA256(req.fields.password + foundUser.salt).toString(encBase64);
+      }
 
-			await foundUser.save();
+      await foundUser.save();
 
-			res.status(200).json(foundUser);
-		}
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
+      res.status(200).json(foundUser);
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = router;
